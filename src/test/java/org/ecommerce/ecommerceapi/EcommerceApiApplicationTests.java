667409bb573ca24b1927c283c9e82c1c9cb02655
italo <166181877+italolira13a@users.noreply.github.com>
2025-06-10 19:01:57 -0300

    test: adiciona testes com Testcontainers e configurações finais do projeto

---
+++ src/test/java/org/ecommerce/ecommerceapi/EcommerceApiApplicationTests.java
@@ -0,0 +1,15 @@
+package org.ecommerce.ecommerceapi;Add commentMore actions
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class EcommerceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/org/ecommerce/ecommerceapi/TestcontainersConfiguration.java
@@ -0,0 +1,11 @@
+package org.ecommerce.ecommerceapi;
+Add commentMore actions
+import org.springframework.boot.SpringApplication;
+
+public class TestEcommerceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(EcommerceApiApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
